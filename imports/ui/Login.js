d693ab72c67a3937b4cb5288a0aa879f3682725d
Will Vincent <will@willvincent.com>
2017-04-14 01:25:28 -0500

    Change login title

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -30,7 +30,7 @@ export default class Login extends React.Component {
     return (
       <div className="boxed-view">
         <div className="boxed-view__box">
-          <h1>Login to ShortLnk</h1>
+          <h1>ShortLnk</h1>
 
           { this.state.error ? <p>{ this.state.error }</p> : undefined }
 
